a5a3d8e9dae0106c7e00759f33b77baf986d061e
Eric Yoon <eyoon525@gmail.com>
2021-12-30 12:29:59 -0800

    returned to non crashing init

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        printf("Needs 3 arguments\n");
+    if (argc != 2) {
+        printf("Needs 2 arguments\n");
         return 1;
     }
     // Initialization
@@ -42,32 +42,32 @@ int main(int argc, char *argv[])
     if (strncmp(argv[1], "0", 1)) {
         localport = 7000;
 
-        players[0].size = sizeof(players[0]);
+        players[0].size = sizeof(GGPOPlayer);
         players[0].player_num = 1;
         players[0].type = GGPO_PLAYERTYPE_LOCAL;
         
-        players[1].size = sizeof(players[1]);
-        players[1].player_num = 2;
+        players[1].size = sizeof(GGPOPlayer);
+        players[1].player_num = 0;
         players[1].type = GGPO_PLAYERTYPE_REMOTE;
         players[1].u.remote.port = 7001;
+        strcpy(players[1].u.remote.ip_address, "127.0.0.1");
+        // strncpy(players[1].u.remote.ip_address, argv[2], sizeof(players[1].u.remote.ip_address)); // TODO: do strncpy later
         
-        strncpy(players[1].u.remote.ip_address, argv[2], sizeof(players[1].u.remote.ip_address));
         
         printf("%s\n", players[1].u.remote.ip_address);
         printf("%d\n", players[1].u.remote.port);
     } else {
         localport = 7001;
 
-        players[1].size = sizeof(players[1]);
-        players[1].player_num = 2;
-        players[1].type = GGPO_PLAYERTYPE_LOCAL;
-        
-        players[0].size = sizeof(players[0]);
-        players[0].player_num = 1;
+        players[0].size = sizeof(GGPOPlayer);
+        players[0].player_num = 0;
         players[0].type = GGPO_PLAYERTYPE_REMOTE;
         players[0].u.remote.port = 7000;
+        strcpy(players[0].u.remote.ip_address, "127.0.0.1");
 
-        strncpy(players[0].u.remote.ip_address, argv[2], sizeof(players[0].u.remote.ip_address));
+        players[1].size = sizeof(GGPOPlayer);
+        players[1].player_num = 1;
+        players[1].type = GGPO_PLAYERTYPE_LOCAL;
 
         printf("%s\n", players[0].u.remote.ip_address);
         printf("%d\n", players[0].u.remote.port);
